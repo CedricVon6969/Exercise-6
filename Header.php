55efa45a45acc78b3f5e593549438eb51fec4aa3
CedricVon6969 <sadocedricvonerrol_bsit@plmun.edu.ph>
2024-10-02 07:50:24 +0800

    Header.php

---
+++ Header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercise 6</title>
+</head>
+<body>
+    <header>
+        <h1>Exercise 6 - PHP HEADER/FOOTER INCLUDES</h1>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="pricing.php">Pricing</a></li>
+                <li><a href="tasks.php">My Tasks</a></li>
+            </ul>
+        </nav>
+    </header>
