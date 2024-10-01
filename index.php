a0e6288272f70cce5673a8f900859852f3cfbf38
CedricVon6969 <sadocedricvonerrol_bsit@plmun.edu.ph>
2024-10-02 07:50:09 +0800

    Index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+header("Content-Type: text/html; charset=UTF-8");
+?>
+
+<?php include 'header.php'; ?>
+
+<main>
+    <h2>Home Page For Exercise 6</h2>
+    <p>This is a sample only for exercise 6</p>
+</main>
+
+<?php include 'footer.php'; ?>
